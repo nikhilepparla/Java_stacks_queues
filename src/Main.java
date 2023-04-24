f59e19c7ceafd30194644e4f085363669aabfadf
nikhilepparla <e.nikhil1999@gmail.com>
2023-04-24 06:57:27 +0530

    Queues in java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,15 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
-        Stack<String> str =new Stack<>();
-        str.push("first");
-        str.push("in");
-        str.push("last");
-        str.push("out");
-        str.pop();
-        System.out.println(str);
+        Queue<Integer> queue = new LinkedList<>();
+        queue.add(10);
+        queue.add(6);
+        queue.add(80);
+        queue.add(7);
+        queue.remove();
+        System.out.println(queue);
     }
 }
